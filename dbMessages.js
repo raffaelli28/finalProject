0f2674acf50918f4643f22ad1424b473b44a6b6f
raffaelli28 <raffaelli.ispas@ucdenver.edu>
2024-05-04 13:28:15 -0600

    new file:   .gitignore 	modified:   dbMessages.js 	deleted:    gitignore

--- dbMessages.js
+++ dbMessages.js
@@ -1,10 +1,10 @@
 import mongoose from 'mongoose'
 
-const messageSchema = mongoose.Schema ({
+const messageSchema = new mongoose.Schema ({
     message: String,
     user: String,
     timeStamp: String,
     received: Boolean
-})
+});
 
 export default mongoose.model('messagingschema', messageSchema)
